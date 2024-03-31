69fb0fee5f66daceb51dc48505dc9a9485ad2d79
Md <mohdmajid9123@gmail.com>
2024-03-31 15:49:58 +0530

    add offer component

--- src/pages/Shop.jsx
+++ src/pages/Shop.jsx
@@ -1,12 +1,14 @@
 import React from "react";
 import Hero from "../conponent/hero/Hero";
 import Popular from "../conponent/popular/Popular";
+import Offer from "../conponent/offer/Offer";
 
 const Shop = () => {
   return (
     <div className="shop">
       <Hero />
       <Popular />
+      <Offer />
     </div>
   );
 };
