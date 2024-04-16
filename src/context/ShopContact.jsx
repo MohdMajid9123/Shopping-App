ce9319a61b8becd348113ad04b4b97317840ad8e
Md <mohdmajid9123@gmail.com>
2024-04-16 16:35:53 +0530

    add cart add or deleted code in ShopContext.jsx component

--- src/context/ShopContact.jsx
+++ src/context/ShopContact.jsx
@@ -1,11 +1,29 @@
-import React, { createContext } from "react";
+import React, { createContext, useState } from "react";
 
 import all_product from "../conponent/Assets/all_product";
 
 export const ShopContact = createContext(null);
 
+const getDefaultCart = () => {
+  let cart = {};
+
+  for (let i = 0; i < all_product.length + 1; i++) {
+    cart[i] = 0;
+  }
+  return cart;
+};
+
 const ShopcontactProvider = (props) => {
-  const contextValue = { all_product };
+  const [cartItems, setCartItems] = useState(getDefaultCart());
+
+  const addToCart = (itemId) => {
+    setCartItems((prev) => ({ ...prev, [itemId]: prev[itemId] + 1 }));
+  };
+  const RemoveToCart = (itemId) => {
+    setCartItems((prev) => ({ ...prev, [itemId]: prev[itemId] - 1 }));
+  };
+
+  const contextValue = { all_product, cartItems, addToCart, RemoveToCart };
 
   return (
     <ShopContact.Provider value={contextValue}>
