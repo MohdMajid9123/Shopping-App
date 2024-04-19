0ddfd2efcd4184fa558e566d02cf5c92a16c275b
Md <mohdmajid9123@gmail.com>
2024-04-19 11:15:13 +0530

    fixed some responsive design

--- src/conponent/item/Item.jsx
+++ src/conponent/item/Item.jsx
@@ -18,8 +18,8 @@ const Item = ({ item }) => {
         <p>{name}</p>
       </Link>
       <div className="item_prices">
-        <div className="item_price_new">{new_price}</div>
-        <div className="item_price_old">{old_price}</div>
+        <div className="item_price_new">${new_price}</div>
+        <div className="item_price_old">${old_price}</div>
       </div>
     </div>
   );
