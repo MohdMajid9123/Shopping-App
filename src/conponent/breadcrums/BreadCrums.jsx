487dd7c5d8b3598df46d8687b3b7f68c50b02f3e
Md <mohdmajid9123@gmail.com>
2024-04-22 15:56:08 +0530

    first commit

--- src/conponent/breadcrums/BreadCrums.jsx
+++ src/conponent/breadcrums/BreadCrums.jsx
@@ -5,7 +5,7 @@ const BreadCrums = (props) => {
   const { product } = props;
   return (
     <div className="breadcrum">
-      HOME <img src={arrow_icon} alt={arrow_icon} /> SHOP{" "}
+      <HOME-screen></HOME-screen> <img src={arrow_icon} alt={arrow_icon} /> SHOP{" "}
       <img src={arrow_icon} alt={arrow_icon} /> {product.category}{" "}
       <img src={arrow_icon} alt={arrow_icon} /> {product.name}
     </div>
